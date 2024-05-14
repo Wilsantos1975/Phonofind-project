1b5232b32e7280d0fa8c832f8b2d4dd9990842df
Your Name <Faustosantos@pursuit.org>
2024-05-13 23:31:50 -0400

    working on post and delete queries

--- backend/queries/vinylQueries.js
+++ backend/queries/vinylQueries.js
@@ -1,68 +1,101 @@
 // query functions for vinyls table in vinyl_collection database
 
-const { get } = require('../app');
-const db = require('../db/index');
-
+const { get } = require("../app");
+const db = require("../db/index");
 
 // get all vinyls
-getAllVinyls = async (req,res) => {
-    try {
-        const allVinyls = await db.any('SELECT * FROM vynils');
-        return allVinyls;
-    } catch (error) {
-        return error;
-    }
-}
+getAllVinyls = async (req, res) => {
+  try {
+    const allVinyls = await db.any("SELECT * FROM vynils");
+    return allVinyls;
+  } catch (error) {
+    return error;
+  }
+};
 
 // get a single vinyl by id
 getSingleVinyl = async (id) => {
-    try {
-        const singleVinyl = await db.one('SELECT * FROM vynils WHERE id = $1', id);
-        console.log(singleVinyl);
-        return singleVinyl;
-
-    } catch (error) {
-        return error;
-    }
-}
+  try {
+    const singleVinyl = await db.one("SELECT * FROM vynils WHERE id = $1", id);
+    console.log(singleVinyl);
+    return singleVinyl;
+  } catch (error) {
+    return error;
+  }
+};
 
 //post a new vinyl
-postNewVinyl = async (req,res) => {
-    try {
-        const { artist, album, year, condition, cover, notes } = req.body;
-        const newVinyl = await db.one('INSERT INTO vynils (artist, album, year, condition, cover, notes) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [artist, album, year, condition, cover, notes]);
-        return newVinyl;
-    } catch (error) {
-        return error;
-    }
-}  
+postNewVinyl = async (req, res) => {
+  try {
+    const {
+      artist,
+      album,
+      release_year,
+      genre,
+      prize,
+      condition,
+      description,
+    } = req.body;
+    const newVinyl = await db.one(
+      "INSERT INTO vynils (artist, album, release_year, genre, prize, condition, description) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+      [artist, album, release_year, genre, prize, condition, description]
+    );
+    return newVinyl;
+  } catch (error) {
+    return error;
+  }
+};
 
 //update vinyl by id
-updateVinyl = async (req,res) => {
-    try {
-        const { id } = req.params;
-        const { artist, album, year, condition, cover, notes } = req.body;
-        const updatedVinyl = await db.one('UPDATE vynils SET artist = $1, album = $2, year = $3, condition = $4, cover = $5, notes = $6 WHERE id = $7 RETURNING *', [artist, album, year, condition, cover, notes, id]);
-        return updatedVinyl;
-    } catch (error) {
-        return error;
-    }
-}
+updateVinyl = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      artist,
+      album,
+      release_year,
+      genre,
+      prize,
+      condition,
+      description,
+    } = req.body;
 
-deleteVinyl = async (req,res) => {
-    try {
-        const { id } = req.params;
-        
-        const deletedVinyl = await db.one('DELETE FROM vynils WHERE id = $1 RETURNING *', Number(id));
-        return deletedVinyl;
-    } catch (error) {
-        return error;
-    }
-}
+    const updatedVinyl = await db.one(
+      `
+        UPDATE vynils
+        SET artist = $1, album = $2, release_year = $3, genre = $4, prize = $5, condition = $6, description = $7
+        WHERE id = $8
+        RETURNING *
+      `,
+      [artist, album, release_year, genre, prize, condition, description, id]
+    );
+
+    return updatedVinyl;
+  } catch (error) {
+    return error;
+  }
+};
 
 
+//delete vinyl by id
+deleteVinyl = async (req, res) => {
+  try {
+    const { id } = req.params;
 
+    const deletedVinyl = await db.one(
+      "DELETE FROM vynils WHERE id = $1 RETURNING *",
+      Number(id)
+    );
+    return deletedVinyl;
+  } catch (error) {
+    return error;
+  }
+};
 
-
-module.exports = {  getAllVinyls, getSingleVinyl, postNewVinyl, updateVinyl, deleteVinyl };
-
+module.exports = {
+  getAllVinyls,
+  getSingleVinyl,
+  postNewVinyl,
+  updateVinyl,
+  deleteVinyl,
+};
