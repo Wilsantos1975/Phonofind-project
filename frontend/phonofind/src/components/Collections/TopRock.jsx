739850724dacffdf9a8efaeb285e5660b5221773
Wilsantos1975 <Faustosantos@pursuit.org>
2024-09-23 22:54:08 -0400

    updated albumcards. working on the joined table.

--- frontend/phonofind/src/components/Collections/TopRock.jsx
+++ frontend/phonofind/src/components/Collections/TopRock.jsx
@@ -1,25 +1,22 @@
+import React, { useMemo } from 'react';
 import AlbumCard from "../common/AlbumCard";
 
+function TopRock({ collections }) {
+    const filteredCollections = useMemo(() => 
+        collections.filter((collection) => collection.album_genre === "Rock"),
+        [collections]
+    );
 
-function TopRock ({collections}) {
-    
-    let filteredCollections = collections.filter((collection) => collection.album_genre === "Rock");
-
-    console.log(filteredCollections, collections);
-    return ( 
-
+    return (
         <div>
             <h1 className="text-5xl text-center text-blue-700">TOP ROCK</h1>
-            <div className="grid grid-cols-1 md:grid-cols-5 lg:grid-cols-9 gap-4">
-            {filteredCollections.map((collection) => {
-                return (
-                <AlbumCard key={collection.album_id} collection={collection} />
-                );  
-            }
-            )}
-                </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+                {filteredCollections.map((collection) => (
+                    <AlbumCard key={collection.album_id} collection={collection} />
+                ))}
             </div>
-     );
+        </div>
+    );
 }
 
 export default TopRock;
